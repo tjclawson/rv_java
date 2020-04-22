d8385c47dddca019b4bcf1bff085cf9f4e7e7d1e
tjclawson <tjamesclawson@gmail.com>
2020-04-22 18:45:25 -0400

    Change Pokemon to Person to use Swapi

---
+++ rv_kotlin/app/src/main/java/com/tjclawson/rv_kotlin/data/Person.kt
@@ -0,0 +1,12 @@
+package com.tjclawson.rv_kotlin.data
+
+data class Person(
+    val name: String,
+    val height: String,
+    val mass: String,
+    val hair_color: String,
+    val skin_color: String,
+    val eye_color: String,
+    val birth_year: String,
+    val gender: String
+)
--- rv_kotlin/app/src/main/java/com/tjclawson/rv_kotlin/data/Pokemon.kt
+++
@@ -1,9 +0,0 @@
-package com.tjclawson.rv_kotlin.data
-
-data class Pokemon(
-    val id: Int,
-    val name: String,
-    val sprites: MutableList<String>,
-    val height: Int,
-    val weight: Int
-)
