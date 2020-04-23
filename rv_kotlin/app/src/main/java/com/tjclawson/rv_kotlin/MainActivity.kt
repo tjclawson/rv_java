32884252c13f8df8c508ce184c4f668054b17275
tjclawson <tjamesclawson@gmail.com>
2020-04-22 21:24:45 -0400

    Add logic to launch list fragment when app is launched

--- rv_kotlin/app/src/main/java/com/tjclawson/rv_kotlin/MainActivity.kt
+++ rv_kotlin/app/src/main/java/com/tjclawson/rv_kotlin/MainActivity.kt
@@ -2,11 +2,19 @@ package com.tjclawson.rv_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
+import com.tjclawson.rv_kotlin.data.ListFragment
+import com.tjclawson.rv_kotlin.data.SharedViewModel
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, ListFragment())
+            .commit()
     }
 }
