e8349e857091f3f61c32aa22db4fda51a26adfee
tjclawson <tjamesclawson@gmail.com>
2020-05-11 20:25:06 -0400

    Add logic to launch appropriate fragments based on screen size

--- master_detail/app/src/main/java/com/tjclawson/master_detail/MainActivity.kt
+++ master_detail/app/src/main/java/com/tjclawson/master_detail/MainActivity.kt
@@ -2,11 +2,34 @@ package com.tjclawson.master_detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: SharedViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(SharedViewModel::class.java)
+
+        if (frame_item_detail != null) viewModel.twoPane = true
+        initUI()
+    }
+
+    private fun initUI() {
+        if (viewModel.twoPane) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_item_list, ListFragment())
+                    .replace(R.id.frame_item_detail, DetailFragment())
+                    .commit()
+        } else {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.list_container, ListFragment())
+                    .commit()
+        }
     }
 }
