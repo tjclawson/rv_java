64ea047deb7f62bd7f66c8d75b034a50af61b593
tjclawson <tjamesclawson@gmail.com>
2020-05-11 20:24:42 -0400

    Add detail fragment

---
+++ master_detail/app/src/main/java/com/tjclawson/master_detail/DetailFragment.kt
@@ -0,0 +1,36 @@
+package com.tjclawson.master_detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.fragment_detail.*
+
+
+class DetailFragment : Fragment() {
+
+    private lateinit var viewModel: SharedViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
+
+        viewModel.selected.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                tv_detail_title.text = it.title
+                tv_detail_description.text = it.description
+            }
+        })
+    }
+}
